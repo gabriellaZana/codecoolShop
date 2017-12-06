db8fb2da9fd0f17bb9de0f855cb15facea79327d
Debora Csomos <csomidebi@gmail.com>
2017-12-06 10:39:58 +0100

    filter works

--- src/main/resources/public/js/script.js
+++ src/main/resources/public/js/script.js
@@ -1,38 +1,42 @@
-categories = $(".clickable-category");
+$(document).ready(function () {
+    let categories = $(".clickable-category");
 
-console.log(categories);
-for (let i = 0; i < categories.length; i++) {
-    if (i > 0) {
-        document.getElementById(`productsOfCategory${categories[i].id.slice(8)}`).style.display = "none";
-    }
-    categories[i].addEventListener('click', function () {
-        productDiv = document.getElementById(`productsOfCategory${this.id.slice(8)}`);
-        console.log(productDiv.style.display);
-        if (productDiv.style.display === "none") {
-            productDiv.style.display = "";
-        } else {
-            productDiv.style.display = "none";
+    for (let i = 0; i < categories.length; i++) {
+        if (i > 0) {
+            document.getElementById(`productsOfCategory${categories[i].id.slice(8)}`).style.display = "none";
         }
-    })
-}
-
-buttons = $('button');
-for (let i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener('click', function () {
-        button = buttons[i];
-        products = button.parent()
-        console.log(typeof products)
-        supplier = button.innerHTML;
-
-        for (let j = 0; j < products.length; i++) {
-            if (supplier === products[j].data('supplier')) {
-                products[j].style.display = '';
+        categories[i].addEventListener('click', function () {
+            let productDiv = document.getElementById(`productsOfCategory${this.id.slice(8)}`);
+            console.log(productDiv.style.display);
+            if (productDiv.style.display === "none") {
+                productDiv.style.display = "";
             } else {
-                products[j].style.display = 'none';
+                productDiv.style.display = "none";
             }
-        }
-    })
-}
+        })
+    }
+
+    let buttons = $('button');
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', function () {
+                let button = $(this);
+                console.log(typeof button);
+                let products = button.parent().parent().find(".product");
+                let supplier = button.html();
+                for (let i = 0; i < products.length; i++) {
+                    let product = products[i];
+                    let productSuppl = product.getAttribute('data-supplier');
+                    if (supplier === productSuppl) {
+                        product.style.display = '';
+                    } else {
+                        product.style.display = 'none';
+                    }
+                }
+            }
+        )
+    }
+})
+;
 
 
 
