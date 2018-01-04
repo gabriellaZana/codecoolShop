2fc215273f6a1fd702c62f72b1be31a486f8755e
Debora Csomos <csomidebi@gmail.com>
2018-01-04 22:46:10 +0100

    rearranged basic test code and overwrote find and add.

--- src/tests/SupplierDaoJDBCTest.java
+++ src/tests/SupplierDaoJDBCTest.java
@@ -1,4 +1,9 @@
 import com.codecool.shop.dao.implementation.JDBC.SupplierDaoJDBC;
+import com.codecool.shop.model.Supplier;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class SupplierDaoJDBCTest extends SupplierDaoTest<SupplierDaoJDBC> {
 
@@ -7,5 +12,22 @@ public class SupplierDaoJDBCTest extends SupplierDaoTest<SupplierDaoJDBC> {
         SupplierDaoJDBC instance = SupplierDaoJDBC.getInstance();
         instance.setFilePath("src/main/resources/sql/config_test.properties");
         return SupplierDaoJDBC.getInstance();
+    }
+
+    @Test
+    @Override
+    public void testFind() {
+        instance.add(objectToTest);
+        Supplier actual = instance.find(objectToTest.getName());
+        assertNotNull(actual);
+    }
+
+    @Test
+    @Override
+    void testAdd() {
+        instance.add(objectToTest);
+        Supplier expected = objectToTest;
+        Supplier actual = instance.find(objectToTest.getName());
+        assertEquals(expected.getName(), actual.getName());
     }
 }
--- src/tests/SupplierDaoTest.java
+++ src/tests/SupplierDaoTest.java
@@ -1,6 +1,6 @@
-
 import com.codecool.shop.dao.SupplierDao;
 import com.codecool.shop.model.Supplier;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,43 +12,54 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 public abstract class SupplierDaoTest<T extends SupplierDao> {
-    private T instance = null;
+    T instance = null;
+    static Supplier objectToTest = null;
     protected abstract T createInstance();
 
+    @BeforeAll
+    static void beforeAll() {
+        objectToTest = new Supplier("1st_test_supplier", "1st_test_supplier_desc");
+        objectToTest.setId(0);
+    }
+
     @BeforeEach
-    public void setup() {
+    void setup() {
         instance = createInstance();
     }
 
+    @AfterEach
+    void cleanup() {
+        instance.remove(objectToTest.getId());
+    }
+
     @Test
-    public void testAdd() {
-        Supplier supp = new Supplier("test_name", "test_description");
-        instance.add(supp);
-        String expected = supp.getName();
-        String actual = instance.find(1).getName();
+    void testAdd() {
+        instance.add(objectToTest);
+        Supplier expected = objectToTest;
+        Supplier actual = instance.find(objectToTest.getId());
         assertEquals(expected, actual);
     }
 
     @Test
-    public void testFind() {
-        Supplier supp = new Supplier("another_test_name", "another_test_desc");
-        instance.add(supp);
-        String expected = supp.getDescription();
-        String actual = instance.find(2).getDescription();
-        assertEquals(expected, actual);
+    void testFind() {
+        instance.add(objectToTest);
+        Supplier actual = instance.find(objectToTest.getId());
+        assertNotNull(actual);
     }
 
     @Test
-    public void testRemove() {
-        instance.remove(2);
-        Supplier actual = instance.find(2);
+    void testRemove() {
+        instance.add(objectToTest);
+        instance.remove(objectToTest.getId());
+        Supplier actual = instance.find(objectToTest.getId());
         assertNull(actual);
     }
 
     @Test
-    public void testGetAll() {
-        List<Supplier> expected = new ArrayList<>(Arrays.asList(instance.find(1)));
+    void testGetAll() {
+        instance.add(objectToTest);
+        List<Supplier> expected = new ArrayList<>(Arrays.asList(objectToTest));
         List<Supplier> actual = instance.getAll();
-        assertEquals(expected.toString(), actual.toString());
+        assertEquals(expected.size(), actual.size());
     }
 }
