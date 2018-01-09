531f281df1ff57ce8c72f1440ded953c6979ea43
gabriellaZana <zanagabriella@gmail.com>
2018-01-09 10:18:30 +0100

    logging added on levels info and debug

--- src/main/java/com/codecool/shop/model/ShoppingCart.java
+++ src/main/java/com/codecool/shop/model/ShoppingCart.java
@@ -3,21 +3,32 @@ package com.codecool.shop.model;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
 public final class ShoppingCart {
+    private static final Logger logger = LoggerFactory.getLogger(ShoppingCart.class);
+
     private static ShoppingCart instance = null;
     private static List<Product> products = new ArrayList<>();
-    private ShoppingCart() {}
+    private ShoppingCart() {
+        logger.debug("Shopping cart has been created.");
+    }
 
     public static ShoppingCart getInstance() {
         if(instance == null){
             instance = new ShoppingCart();
         }
+        logger.debug("Shopping cart already exists.");
         return instance;
     }
 
 
     public void putProductToCart(Product product){
         products.add(product);
+        logger.info("Product - {} - has been added to your cart.", product.name);
+        logger.info("Quantity of this product in cart is: {}.", ShoppingCart.getInstance().getQuantity(product));
     }
 
     public List<Product> getProductsFromCart(){
@@ -32,18 +43,22 @@ public final class ShoppingCart {
                 quant ++;
             }
         }
+        logger.info("Quantity of this product in cart is: {}.", quant);
         return quant;
     }
 
     public void deleteItemFromCard(int id){
+        logger.debug("Id of the product which the customer'd like to delete is: {}.", id);
         for (int i=0; i<products.size(); i++) {
             if(products.get(i).id == id){
                 products.remove(i);
             }
         }
+        logger.info("Item has been removed from cart.");
     }
 
     public void removeAllItem(){
         products.clear();
+        logger.info("All item has been removed from cart.");
     }
 }
