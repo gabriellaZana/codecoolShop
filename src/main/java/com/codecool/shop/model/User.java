7b09833a36ca74e54ce7ae4aebc7b8730232f60e
gabriellaZana <zanagabriella@gmail.com>
2018-01-16 11:27:07 +0100

    user class, userDao interface, userDaoJDBC added

---
+++ src/main/java/com/codecool/shop/model/User.java
@@ -0,0 +1,64 @@
+package com.codecool.shop.model;
+
+public class User {
+    private String email, password, shipping_address, billing_address, firstname, lastname;
+
+
+    public User(String email, String password, String shipping_address, String billing_address, String firstname, String lastname) {
+        this.email = email;
+        this.password = password;
+        this.billing_address = billing_address;
+        this.shipping_address = shipping_address;
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getShipping_address() {
+        return shipping_address;
+    }
+
+    public void setShipping_address(String shipping_address) {
+        this.shipping_address = shipping_address;
+    }
+
+    public String getBilling_address() {
+        return billing_address;
+    }
+
+    public void setBilling_address(String billing_address) {
+        this.billing_address = billing_address;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+}
