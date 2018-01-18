7abb204517e91a3353ccd0e499604ca98c3810e9
Debora Csomos <csomidebi@gmail.com>
2018-01-18 13:58:15 +0100

    Payment works, cancel, authorize and error methods done.

--- src/main/resources/public/js/paypal.js
+++ src/main/resources/public/js/paypal.js
@@ -1,3 +1,5 @@
+let price = 0;
+
 paypal.Button.render({
     env: 'sandbox', // Or 'production',
 
@@ -14,44 +16,61 @@ paypal.Button.render({
     // PayPal Client IDs - replace with your own
     // Create a PayPal app: https://developer.paypal.com/developer/applications/create
     client: {
-        sandbox:    'ASI6dDgJRqWo1cRY4nPLxGDg2qR6ZAmHdx69bm_PgTiHyVBYI66ga41BC2mm0IRB_s9yL63mfUT78YbQ',
+        sandbox: 'ASI6dDgJRqWo1cRY4nPLxGDg2qR6ZAmHdx69bm_PgTiHyVBYI66ga41BC2mm0IRB_s9yL63mfUT78YbQ',
         production: '<insert production client id>'
     },
 
-    payment: function(data, actions) {
-        let totalPrice = 0;
-        $('.finalPrice').each(function (i, obj) {
-            totalPrice += parseFloat(obj.innerHTML);
+    payment: function (data, actions) {
+        $.ajax({
+            url: 'shopping-cart-total-price',
+            type: 'GET',
+            contentType: 'application/json; charset=UTF-8',
+            async: false,
+            success: function (response) {
+                price = response;
+            },
+            error: function (e) {
+                console.log(e);
+                alert("Sorry, something went wrong!");
+            }
         });
-        console.log(totalPrice);
         return actions.payment.create({
             payment: {
                 transactions: [
                     {
-                        amount: { total: String(totalPrice), currency: 'USD' }
+                        amount: {total: price, currency: 'USD'}
                     }
                 ]
             }
         });
+
     },
 
-    onAuthorize: function(data, actions) {
+    onAuthorize: function (data, actions) {
         // Make a call to the REST api to execute the payment
-        return actions.payment.execute().then(function() {
+        return actions.payment.execute().then(function () {
             window.alert('Payment Complete!');
-            $(location).attr('href', '/');
+            $.ajax({
+                url: 'empty-cart',
+                type: 'GET',
+                contentType: 'application/json; charset=UTF-8',
+                success: function(response) {
+                    $(location).attr('href', '/');
+                }
+            })
         });
     },
 
-    onCancel: function(data, actions) {
-        /*
-         * Buyer cancelled the payment
-         */
+    onCancel: function (data, actions) {
+        alert("Payment cancelled!");
     },
 
-    onError: function(err) {
-        /*
-         * An error occurred during the transaction
-         */
+    onError: function (err) {
+        if (price === "0.0") {
+            alert("There's nothing in your cart!");
+        } else {
+            console.log(price);
+            alert("Sorry, something went wrong!");
+        }
     }
 }, '#paypal-button');
